27b2da22dc0b16a73b88de7dcda39dc3b9930a8a
remon3939 <123156895+remon3939@users.noreply.github.com>
2023-09-12 19:54:07 +0900

    initial commit
    練習

---
+++ 練習/js/java.js
@@ -0,0 +1,8 @@
+'use strict';
+
+window.history.pushState(null, document.title, location.href);
+
+window.addEventListener('popstate', function (event) {
+
+  window.history.pushState(null, document.title, location.href);
+});
